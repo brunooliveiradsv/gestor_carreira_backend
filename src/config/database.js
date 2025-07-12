c519533958a753ad456d65ef3a88cb03a9b0b354
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-12 12:12:16 -0300

    db

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  url: process.env.DATABASE_URL, // Voltar a usar a variável de ambiente
+  url: process.env.DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
@@ -12,6 +12,9 @@ module.exports = {
     ssl: {
       require: true,
       rejectUnauthorized: false
-    }
+    },
+    // --- NOVA LINHA PARA FORÇAR O USO DE IPv4 ---
+    family: 4
+    // -----------------------------------------
   }
 };
