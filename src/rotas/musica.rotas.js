17a12c13d7055de78a6dce8024af01cdc7611ff3
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-15 11:26:53 -0300

    Ok

--- src/rotas/musica.rotas.js
+++ src/rotas/musica.rotas.js
@@ -7,19 +7,15 @@ module.exports = (conexao) => {
   const roteador = express.Router();
   roteador.use(authMiddleware(conexao));
 
-  // --- Rotas Gerais (sem parâmetros na URL) ---
-  // Estas rotas são mais específicas e vêm primeiro.
-  
+  // --- Rotas Gerais (sem parâmetros) ---
+  // Estas são as rotas mais específicas e devem vir primeiro.
   roteador.get('/', (req, res) => musicaControlador.listar(req, res, conexao));
   roteador.post('/', (req, res) => musicaControlador.criar(req, res, conexao));
   roteador.post('/raspar-cifra', (req, res) => musicaControlador.rasparCifra(req, res, conexao));
-
-  // --- ROTA FALTANDO ADICIONADA AQUI ---
   roteador.post('/busca-inteligente', (req, res) => musicaControlador.buscaInteligente(req, res, conexao));
-  
-  // --- Rotas Específicas (com o parâmetro /:id) ---
-  // O Express só chegará a estas rotas se o caminho não corresponder às de cima.
-  
+
+  // --- Rotas com Parâmetro /:id ---
+  // O Express só chegará a estas se o caminho não corresponder às de cima.
   roteador.get('/:id', (req, res) => musicaControlador.buscarPorId(req, res, conexao));
   roteador.put('/:id', (req, res) => musicaControlador.atualizar(req, res, conexao));
   roteador.delete('/:id', (req, res) => musicaControlador.apagar(req, res, conexao));
