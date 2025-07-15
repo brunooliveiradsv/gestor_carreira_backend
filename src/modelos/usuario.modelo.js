e248e23d38073dfdddb91d558e8518def224cf32
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-14 22:29:31 -0300

    Fix

--- src/modelos/usuario.modelo.js
+++ src/modelos/usuario.modelo.js
@@ -9,9 +9,11 @@ class Usuario extends Model {
     this.hasMany(models.Compromisso, { foreignKey: 'usuario_id', as: 'compromissos' });
     this.hasMany(models.Contato, { foreignKey: 'usuario_id', as: 'contatos' });
     this.hasMany(models.Transacao, { foreignKey: 'usuario_id', as: 'transacoes' });
-    this.hasMany(models.Repertorio, { foreignKey: 'usuario_id', as: 'repertorios' });
     this.hasMany(models.Notificacao, { foreignKey: 'usuario_id', as: 'notificacoes' });
     this.hasMany(models.Equipamento, { foreignKey: 'usuario_id', as: 'equipamentos' });
+    this.hasMany(models.Setlist, { foreignKey: 'usuario_id', as: 'setlists' });
+    this.hasMany(models.Musica, { foreignKey: 'usuario_id', as: 'musicas' });
+    this.hasMany(models.Tag, { foreignKey: 'usuario_id', as: 'tags' });
     this.belongsToMany(models.Conquista, { through: models.UsuarioConquista, foreignKey: 'usuario_id', as: 'conquistas' });
     
   }
