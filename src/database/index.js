0b7a7f9f5d37e95cba9b1998df47264a8a33301b
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-14 22:40:10 -0300

    Index

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
-// Importa todas as definições de modelo
+// Importa TODOS os modelos da aplicação
 const Usuario = require('../modelos/usuario.modelo');
 const Compromisso = require('../modelos/compromisso.modelo');
 const Transacao = require('../modelos/transacao.modelo.js');
@@ -11,40 +11,39 @@ const Conquista = require('../modelos/conquista.modelo.js');
 const UsuarioConquista = require('../modelos/usuario_conquista.modelo.js');
 const Notificacao = require('../modelos/notificacao.modelo.js');
 const Equipamento = require('../modelos/equipamento.modelo.js');
-const Musica = require('../modelos/musica.modelo');
+const Setlist = require('../modelos/setlist.modelo.js');
+const Musica = require('../modelos/musica.modelo.js');
 const Tag = require('../modelos/tag.modelo.js');
-const Setlist = require('../modelos/setlist.modelo.js'); // Atualizado
-const SetlistMusica = require('../modelos/setlist_musica.modelo.js'); // Novo
+const SetlistMusica = require('../modelos/setlist_musica.modelo.js');
+
 
 const conexao = new Sequelize(dbConfig);
 
-// Inicializa cada modelo
-Usuario.init(conexao);
-Compromisso.init(conexao);
-Transacao.init(conexao);
-Contato.init(conexao);
-Conquista.init(conexao);
-UsuarioConquista.init(conexao);
-Notificacao.init(conexao);
-Equipamento.init(conexao);
-Musica.init(conexao);
-Tag.init(conexao);
-Setlist.init(conexao);
-SetlistMusica.init(conexao);
+// Coloca todos os modelos num array para facilitar a gestão
+const modelos = [
+  Usuario,
+  Compromisso,
+  Transacao,
+  Contato,
+  Conquista,
+  UsuarioConquista,
+  Notificacao,
+  Equipamento,
+  Setlist,
+  Musica,
+  Tag,
+  SetlistMusica,
+];
 
-// Executa as associações de cada modelo
-Usuario.associate(conexao.models);
-Compromisso.associate(conexao.models);
-Transacao.associate(conexao.models);
-Contato.associate(conexao.models);
-Conquista.associate(conexao.models);
-UsuarioConquista.associate(conexao.models);
-Notificacao.associate(conexao.models);
-Equipamento.associate(conexao.models);
-Musica.associate(conexao.models);
-Tag.associate(conexao.models);
-Setlist.associate(conexao.models);
-SetlistMusica.associate(conexao.models);
+// Inicializa cada modelo, passando a conexão
+modelos.forEach(modelo => modelo.init(conexao));
 
+// --- CÓDIGO CORRIGIDO ---
+// Executa as associações de cada modelo, APENAS se o método 'associate' existir
+modelos.forEach(modelo => {
+  if (modelo.associate) {
+    modelo.associate(conexao.models);
+  }
+});
 
 module.exports = conexao;
