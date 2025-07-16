13311bdb3f6794031ba1b5c200aaa7d4d4990d0a
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-16 13:37:24 -0300

    Fix

--- src/rotas/admin.rotas.js
+++ src/rotas/admin.rotas.js
@@ -1,6 +1,8 @@
 // src/rotas/admin.rotas.js
 const express = require('express');
 const adminControlador = require('../controladores/admin.controlador');
+// --- LINHA ADICIONADA ---
+const sugestaoControlador = require('../controladores/sugestao.controlador'); 
 const authMiddleware = require('../middlewares/autenticacao');
 const adminMiddleware = require('../middlewares/admin');
 
@@ -14,11 +16,10 @@ module.exports = (conexao) => {
   roteador.get('/usuarios', (req, res) => adminControlador.listarUsuarios(req, res, conexao));
   roteador.put('/usuarios/:id', (req, res) => adminControlador.atualizarUsuario(req, res, conexao));
   roteador.delete('/usuarios/:id', (req, res) => adminControlador.apagarUsuario(req, res, conexao));
-  
-  // --- ROTA POST QUE ESTAVA FALTANDO ---
   roteador.post('/usuarios', (req, res) => adminControlador.criarUsuario(req, res, conexao));
   roteador.delete('/usuarios/:id/dados', (req, res) => adminControlador.limparDadosUsuario(req, res, conexao));
 
+  // --- ROTAS DE MODERAÇÃO DE SUGESTÕES ---
   roteador.get('/sugestoes', (req, res) => sugestaoControlador.listarSugestoesPendentes(req, res, conexao));
   roteador.put('/sugestoes/:id/aprovar', (req, res) => sugestaoControlador.aprovarSugestao(req, res, conexao));
   roteador.put('/sugestoes/:id/rejeitar', (req, res) => sugestaoControlador.rejeitarSugestao(req, res, conexao));
