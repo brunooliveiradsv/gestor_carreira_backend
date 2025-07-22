6011b1e516e635638b599b3ff24bb4e220490374
Bruno Oliveira <142401392+brunooliveiradsv@users.noreply.github.com>
2025-07-22 16:21:06 -0300

    Atualizar o setlist.rotas.js

--- src/rotas/setlist.rotas.js
+++ src/rotas/setlist.rotas.js
@@ -9,31 +9,18 @@ module.exports = (conexao) => {
   roteador.use(authMiddleware(conexao));
 
   // --- ROTAS DO DASHBOARD E GERAIS ---
-  // Rota para as estatísticas da "Central de Comando"
-  roteador.get('/estatisticas', (req, res) => setlistControlador.estatisticas(req, res, conexao));
-  
-  // Lista todos os setlists do usuário
-  roteador.get('/', (req, res) => setlistControlador.listar(req, res, conexao));
-  
-  // Cria um novo setlist (ainda sem músicas)
-  roteador.post('/', assinaturaMiddleware(), (req, res) => setlistControlador.criar(req, res, conexao));
+  roteador.get('/estatisticas', (req, res, next) => setlistControlador.estatisticas(req, res, conexao, next));
+  roteador.get('/', (req, res, next) => setlistControlador.listar(req, res, conexao, next));
+  roteador.post('/', assinaturaMiddleware(), (req, res, next) => setlistControlador.criar(req, res, conexao, next));
 
   // --- ROTAS ESPECÍFICAS DE UM SETLIST ---
-  // Busca um setlist específico, já com as músicas ordenadas
-  roteador.get('/:id', (req, res) => setlistControlador.buscarPorId(req, res, conexao));
-  
-  // Atualiza os dados básicos de um setlist (nome, notas, etc.)
-  roteador.put('/:id', (req, res) => setlistControlador.atualizar(req, res, conexao));
-  
-  // Apaga um setlist e suas associações
-  roteador.delete('/:id', (req, res) => setlistControlador.apagar(req, res, conexao));
+  roteador.get('/:id', (req, res, next) => setlistControlador.buscarPorId(req, res, conexao, next));
+  roteador.put('/:id', (req, res, next) => setlistControlador.atualizar(req, res, conexao, next));
+  roteador.delete('/:id', (req, res, next) => setlistControlador.apagar(req, res, conexao, next));
 
   // --- ROTAS DE MANIPULAÇÃO DE MÚSICAS NO SETLIST ---
-  // Adiciona, remove e reordena as músicas de um setlist
-  roteador.put('/:id/musicas', (req, res) => setlistControlador.atualizarMusicas(req, res, conexao));
-  
-  // A "mágica": sugere músicas para completar um setlist
-  roteador.post('/:id/sugerir', (req, res) => setlistControlador.sugerirMusicas(req, res, conexao));
+  roteador.put('/:id/musicas', (req, res, next) => setlistControlador.atualizarMusicas(req, res, conexao, next));
+  roteador.post('/:id/sugerir', (req, res, next) => setlistControlador.sugerirMusicas(req, res, conexao, next));
 
   return roteador;
 };
