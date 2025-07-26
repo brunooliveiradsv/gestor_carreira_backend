49ff076c7e3cad9bfdcf6f04536ccb136028759e
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-26 15:57:24 -0300

    ok

--- src/rotas/vitrine.rotas.js
+++ src/rotas/vitrine.rotas.js
@@ -6,14 +6,9 @@ const enqueteControlador = require('../controladores/enquete.controlador');
 module.exports = (conexao) => {
   const roteador = express.Router();
 
-  roteador.get('/:url_unica', (req, res) => vitrineControlador.obterVitrine(req, res, conexao));
-  roteador.post('/:url_unica/aplaudir', (req, res) => vitrineControlador.registrarAplauso(req, res, conexao));
-
-  // --- ROTA NOVA ---
-  roteador.post('/posts/:id/reacao', (req, res) => vitrineControlador.registrarReacaoPost(req, res, conexao));
-
-   roteador.post('/enquetes/votar/:idOpcao', (req, res, next) => enqueteControlador.votarEmOpcao(req, res, conexao, next));
-
-
+  roteador.get('/:url_unica', (req, res, next) => vitrineControlador.obterVitrine(req, res, conexao, next));
+  roteador.post('/:url_unica/aplaudir', (req, res, next) => vitrineControlador.registrarAplauso(req, res, conexao, next));
+  roteador.post('/posts/:id/reacao', (req, res, next) => vitrineControlador.registrarReacaoPost(req, res, conexao, next));
+  roteador.post('/enquetes/votar/:idOpcao', (req, res, next) => enqueteControlador.votarEmOpcao(req, res, conexao, next));
   return roteador;
 };
