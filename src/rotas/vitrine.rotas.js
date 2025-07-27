ea8080f8dc15e91b7b6e98e1b8c886ce5eb5068c
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-27 00:11:03 -0300

    Ok

--- src/rotas/vitrine.rotas.js
+++ src/rotas/vitrine.rotas.js
@@ -10,8 +10,14 @@ const authFaMiddleware = require('../middlewares/authFa');
 module.exports = (conexao) => {
   const roteador = express.Router();
 
-  // --- ROTAS PÚBLICAS (não exigem login de fã) ---
-  // Middleware para encontrar o artista é aplicado primeiro
+  // --- ROTA ESPECÍFICA MOVIDA PARA CIMA ---
+  // Esta rota agora é verificada ANTES da rota genérica /:url_unica
+  roteador.get('/meus-likes',
+    authFaMiddleware(conexao),
+    (req, res, next) => vitrineControlador.obterLikesDoFa(req, res, conexao, next)
+  );
+
+  // --- ROTAS PÚBLICAS (com parâmetro :url_unica) ---
   roteador.get('/:url_unica', 
     (req, res, next) => encontrarArtistaPorUrl(req, res, conexao, next),
     (req, res, next) => vitrineControlador.obterVitrine(req, res, conexao, next)
@@ -26,37 +32,28 @@ module.exports = (conexao) => {
     (req, res, next) => encontrarArtistaPorUrl(req, res, conexao, next),
     (req, res, next) => vitrineControlador.obterMusicasMaisCurtidas(req, res, conexao, next)
   );
-
+  
   // --- ROTAS PROTEGIDAS (exigem login de fã) ---
   
-  // Rota de aplauso
   roteador.post('/:url_unica/aplaudir',
-    authFaMiddleware(conexao), // 1. Verifica o token do fã
-    (req, res, next) => encontrarArtistaPorUrl(req, res, conexao, next), // 2. Encontra o artista
-    (req, res, next) => vitrineControlador.registrarAplauso(req, res, conexao, next) // 3. Executa a ação
+    authFaMiddleware(conexao),
+    (req, res, next) => encontrarArtistaPorUrl(req, res, conexao, next),
+    (req, res, next) => vitrineControlador.registrarAplauso(req, res, conexao, next)
   );
 
-  // Rota de "gosto/não gosto"
   roteador.post('/posts/:id/reacao',
-    authFaMiddleware(conexao), // 1. Garante que um fã está logado
-    (req, res, next) => vitrineControlador.registrarReacaoPost(req, res, conexao, next) // 2. Executa a ação
+    authFaMiddleware(conexao),
+    (req, res, next) => vitrineControlador.registrarReacaoPost(req, res, conexao, next)
   );
 
-  // Rota para curtir uma música
   roteador.post('/musicas/:id/like',
     authFaMiddleware(conexao),
     (req, res, next) => vitrineControlador.likeMusica(req, res, conexao, next)
   );
 
-  // ROTA DE VOTAÇÃO MOVIDA PARA AQUI
   roteador.post('/enquetes/votar/:idOpcao', 
-    authFaMiddleware(conexao), // Middleware de autenticação de fã adicionado
-    (req, res, next) => enqueteControlador.votarEmOpcao(req, res, conexao, next)
-  );
-
-  roteador.get('/meus-likes',
     authFaMiddleware(conexao),
-    (req, res, next) => vitrineControlador.obterLikesDoFa(req, res, conexao, next)
+    (req, res, next) => enqueteControlador.votarEmOpcao(req, res, conexao, next)
   );
   
   return roteador;
